20f6f29f5a9f887719b6d0cd1428528aa52e2891
wo80 <christian.woltering@tu-dortmund.de>
2020-12-19 18:00:57 +0100

    Make constants const.

--- src/ARPACK/arpack-ng/arpack_constants.h
+++ src/ARPACK/arpack-ng/arpack_constants.h
@@ -1,46 +1,54 @@
 #pragma once
 
-static logical c_true = TRUE_;
-static logical c_false = FALSE_;
+/*
+ * NOTE:
+ *
+ * Putting the constants in a header means that each translation unit (c file)
+ * including the header will get its own set of constants.
+ * 
+ */
 
-static integer c__0 = 0;
-static integer c__1 = 1;
-static integer c__2 = 2;
-static integer c__3 = 3;
-static integer c__4 = 4;
+static const logical c_true = TRUE_;
+static const logical c_false = FALSE_;
 
-static complex c_one = { 1.0f, 0.0f };
-static complex c_zero = { 0.0f, 0.0f };
+static const integer c__0 = 0;
+static const integer c__1 = 1;
+static const integer c__2 = 2;
+static const integer c__3 = 3;
+static const integer c__4 = 4;
 
-static real s_one = 1.0f;
-static real s_zero = 0.0f;
-static real s_m1 = -1.0f;
+static const complex c_one = { 1.0f, 0.0f };
+static const complex c_zero = { 0.0f, 0.0f };
 
-static doublereal d_one = 1.0;
-static doublereal d_zero = 0.0;
-static doublereal d_m1 = -1.0;
-static doublereal d_23 = 0.66666666666666663;
+static const real s_one = 1.0f;
+static const real s_zero = 0.0f;
+static const real s_m1 = -1.0f;
 
-static doublecomplex z_one = { 1.0, 0.0 };
-static doublecomplex z_zero = { 0.0, 0.0 };
+static const doublereal d_one = 1.0;
+static const doublereal d_zero = 0.0;
+static const doublereal d_m1 = -1.0;
+static const doublereal d_23 = 0.66666666666666663;
+
+static const doublecomplex z_one = { 1.0, 0.0 };
+static const doublecomplex z_zero = { 0.0, 0.0 };
 
 /* used in examples */
 
-static integer c__5 = 5;
-static integer c__6 = 6;
-static integer c__9 = 9;
-static integer c__25 = 25;
-static integer c__30 = 30;
-static integer c__250 = 250;
-static integer c__256 = 256;
-static integer c_n6 = -6;
+static const integer c__5 = 5;
+static const integer c__6 = 6;
+static const integer c__9 = 9;
+static const integer c__25 = 25;
+static const integer c__30 = 30;
+static const integer c__250 = 250;
+static const integer c__256 = 256;
+static const integer c_n6 = -6;
 
-static complex c_two = { 2.0f, 0.0f };
-static complex c_four = { 4.0f, 0.0f };
-static complex c_six = { 6.0f, 0.0f };
-static complex c_ten = { 10.0f, 0.0f };
+static const complex c_two = { 2.0f, 0.0f };
+static const complex c_four = { 4.0f, 0.0f };
+static const complex c_six = { 6.0f, 0.0f };
+static const complex c_ten = { 10.0f, 0.0f };
 
-static doublecomplex z_two = { 2.0, 0.0 };
-static doublecomplex z_four = { 4.0, 0.0 };
-static doublecomplex z_six = { 6.0, 0.0 };
-static doublecomplex z_ten = { 10.0, 0.0 };
+static const doublecomplex z_two = { 2.0, 0.0 };
+static const doublecomplex z_four = { 4.0, 0.0 };
+static const doublecomplex z_six = { 6.0, 0.0 };
+static const doublecomplex z_ten = { 10.0, 0.0 };
