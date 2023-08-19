fdd84a6ada18d08616ac38284452a769bdf83937
wo80 <christian.woltering@tu-dortmund.de>
2023-08-19 10:23:58 +0200

    Add arpack-ng version info to arpack.h

--- src/ARPACK/arpack-ng/arpack.h
+++ src/ARPACK/arpack-ng/arpack.h
@@ -8,9 +8,17 @@
 #include "lapack.h"
 #include "arpack_internal.h"
 
+/* Legacy ARPACK version */
 #define ARPACK_VERSION "2.4.0"
 #define ARPACK_DATE "07/31/1996"
 
 #define ARPACK_VERSION_MAJOR 2
 #define ARPACK_VERSION_MINOR 4
 #define ARPACK_VERSION_PATCH 0
+
+/* ARPACK NG version */
+#define arpack_ng_VERSION "3.9.1"
+
+#define arpack_ng_VERSION_MAJOR 3
+#define arpack_ng_VERSION_MINOR 9
+#define arpack_ng_VERSION_PATCH 1
